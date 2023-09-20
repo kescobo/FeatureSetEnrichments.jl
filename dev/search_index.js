b5a356bf413d663d3c91e180751df91eec6a1bf9
Documenter.jl <documenter@juliadocs.github.io>
2023-09-20 15:02:15 +0000

    build based on 1a41ce8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FeatureSetEnrichments","category":"page"},{"location":"#FeatureSetEnrichments","page":"Home","title":"FeatureSetEnrichments","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FeatureSetEnrichments.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FeatureSetEnrichments]","category":"page"}]
+}
